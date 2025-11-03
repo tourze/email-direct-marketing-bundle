5b91702d45e3dd840035a7cc070e6eda68996ce7
github-ci <you@example.com>
2025-11-03 14:20:03 +0800

    update++++++++++++

--- src/EmailDirectMarketingBundle.php
+++ src/EmailDirectMarketingBundle.php
@@ -5,6 +5,7 @@ namespace EmailDirectMarketingBundle;
 use Doctrine\Bundle\DoctrineBundle\DoctrineBundle;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 use Tourze\BundleDependency\BundleDependencyInterface;
+use Tourze\EasyAdminMenuBundle\EasyAdminMenuBundle;
 
 class EmailDirectMarketingBundle extends Bundle implements BundleDependencyInterface
 {
@@ -12,6 +13,7 @@ class EmailDirectMarketingBundle extends Bundle implements BundleDependencyInter
     {
         return [
             DoctrineBundle::class => ['all' => true],
+            EasyAdminMenuBundle::class => ['all' => true],
         ];
     }
 }
